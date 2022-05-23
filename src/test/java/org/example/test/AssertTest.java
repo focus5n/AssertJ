f2a2829eb76bf7db941bd865b824794ac840c07d
hans <focus5n@naver.com>
2022-05-23 13:00:54 +0900

    Lambda + extractProperty

--- src/test/java/org/example/test/AssertTest.java
+++ src/test/java/org/example/test/AssertTest.java
@@ -1,7 +1,11 @@
 package org.example.test;
 
+import org.example.AssertJ.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -33,9 +37,55 @@ public class AssertTest {
                 .isEqualTo(3.14);
     }
 
-//    @Test
-//    @DisplayName("Chaining Test -Object")
-//    void chainingTestUser() {
-//        assertThat()
-//    }
+    @Test
+    @DisplayName("Chaining Test -Object")
+    void chainingTestUser() {
+        User user = new User("Hans", 33);
+
+        assertThat(user.getAge())
+                .as("check1 %s's age", user.getName())
+                .isEqualTo(33);
+    }
+
+    @Test
+    @DisplayName("Filtering Test with Lambda")
+    void lambdaTest() {
+        List<User> userList = new ArrayList<>();
+        User admin = new User("admin", 30);
+        User user0 = new User("user0", 10);
+        User user1 = new User("user1", 20);
+
+        userList.add(admin);
+        userList.add(user0);
+        userList.add(user1);
+
+        assertThat(userList)
+                .filteredOn(user -> user.getName().contains("admin"))
+                .contains(admin);
+
+        assertThat(userList)
+                .filteredOn("age", notIn(30))
+                .containsOnly(user0, user1);
+    }
+    
+    @Test
+    @DisplayName("Property 추출 테스트")
+    void extractPropertyTest() {
+        List<User> userList = new ArrayList<>();
+        User admin = new User("admin", 30);
+        User user0 = new User("user0", 10);
+        User user1 = new User("user1", 20);
+        userList.add(admin);
+        userList.add(user0);
+        userList.add(user1);
+
+        assertThat(userList)
+                .extracting("name", "age")
+                .contains(tuple("admin", 30),
+                          tuple("user0", 10),
+                          tuple("user1", 20));
+    }
+
+
+
 }
