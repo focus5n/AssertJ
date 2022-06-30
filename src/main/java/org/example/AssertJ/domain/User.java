208a43f630a945309f34a1c341208e5d8ee816bd
hans <focus5n@naver.com>
2022-06-30 22:14:16 +0900

    버그 수정

--- src/main/java/org/example/AssertJ/domain/User.java
+++ src/main/java/org/example/AssertJ/domain/User.java
@@ -1,8 +1,12 @@
 package org.example.AssertJ.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     String name;
     Integer age;
