992a14f53558a66b4594a0aa8ab93ed0fcbd7fb4
hans <focus5n@naver.com>
2022-06-30 22:13:19 +0900

    롬복 적용

--- src/main/java/org/example/AssertJ/domain/User.java
+++ src/main/java/org/example/AssertJ/domain/User.java
@@ -1,19 +1,9 @@
 package org.example.AssertJ.domain;
 
+import lombok.Data;
+
+@Data
 public class User {
     String name;
     Integer age;
-
-    public User() {}
-
-    public User(String name, Integer age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() { return name; }
-    public Integer getAge() { return age; }
-
-    public void setName(String name) { this.name = name; }
-    public void setAge(Integer age) { this.age = age; }
 }
